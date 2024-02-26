1fb93a61960e7b6789b6ffce21e1fd98573dcd48
Pete Letkeman <pete@letkeman.ca>
2024-02-26 18:49:07 -0500

    fixed get exercise listing with pagination

--- controllers/exercise_controller.py
+++ controllers/exercise_controller.py
@@ -1,27 +1,19 @@
 from __future__ import annotations
 
-from datetime import date
 from typing import TYPE_CHECKING
-from uuid import UUID
 
 from litestar.exceptions import HTTPException
-from litestar.repository._filters import OrderBy
-from pydantic import BaseModel as _BaseModel
-from pydantic import TypeAdapter
-from sqlalchemy import ForeignKey, select
-from sqlalchemy.orm import Mapped, mapped_column, relationship, selectinload
+from litestar.pagination import OffsetPagination
+from litestar.repository.filters import OrderBy
 
-from litestar import Litestar, get, HttpMethod, route, status_codes
-from litestar.contrib.sqlalchemy.base import UUIDAuditBase, UUIDBase
-from litestar.contrib.sqlalchemy.plugins import AsyncSessionConfig, SQLAlchemyAsyncConfig, SQLAlchemyInitPlugin
+from litestar import get, HttpMethod, route, status_codes
 from litestar.contrib.sqlalchemy.repository import SQLAlchemyAsyncRepository
 from litestar.controller import Controller
 from litestar.di import Provide
-from litestar.handlers.http_handlers.decorators import delete, patch, post
-from litestar.pagination import OffsetPagination
+from litestar.handlers.http_handlers.decorators import delete, post
 from litestar.params import Parameter
 from litestar.repository.filters import LimitOffset
-
+from pydantic import TypeAdapter
 
 if TYPE_CHECKING:
     from sqlalchemy.ext.asyncio import AsyncSession
@@ -40,6 +32,29 @@ async def provide_exercise_repo(db_session: AsyncSession) -> ExerciseRepository:
     """This provides a simple example demonstrating how to override the join options
     for the repository."""
     return ExerciseRepository(session=db_session)
+
+
+def provide_limit_offset_pagination(
+    current_page: int = Parameter(ge=1, query="currentPage", default=1, required=False),
+    page_size: int = Parameter(
+        query="pageSize",
+        ge=1,
+        default=10,
+        required=False,
+    ),
+) -> LimitOffset:
+    """Add offset/limit pagination.
+
+    Return type consumed by `Repository.apply_limit_offset_pagination()`.
+
+    Parameters
+    ----------
+    current_page : int
+        LIMIT to apply to select.
+    page_size : int
+        OFFSET to apply to select.
+    """
+    return LimitOffset(page_size, page_size * (current_page - 1))
 
 
 class ExerciseController(Controller):
--- main.py
+++ main.py
@@ -15,7 +15,7 @@ from litestar.template import TemplateConfig
 from litestar.contrib.sqlalchemy.plugins import AsyncSessionConfig, SQLAlchemyAsyncConfig, SQLAlchemyInitPlugin
 import logging
 
-from controllers.exercise_controller import ExerciseController
+from controllers.exercise_controller import ExerciseController, provide_limit_offset_pagination
 from controllers.my_controller import MyAPIController
 from models.base_model import Base
 from models.exercise import Exercise
@@ -48,29 +48,6 @@ async def on_startup() -> None:
     """Initializes the database."""
     async with sqlalchemy_config.get_engine().begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
-
-
-def provide_limit_offset_pagination(
-        current_page: int = Parameter(ge=1, query="currentPage", default=1, required=False),
-        page_size: int = Parameter(
-            query="pageSize",
-            ge=1,
-            default=10,
-            required=False,
-        ),
-) -> LimitOffset:
-    """Add offset/limit pagination.
-
-    Return type consumed by `Repository.apply_limit_offset_pagination()`.
-
-    Parameters
-    ----------
-    current_page : int
-        LIMIT to apply to select.
-    page_size : int
-        OFFSET to apply to select.
-    """
-    return LimitOffset(page_size, page_size * (current_page - 1))
 
 
 app = Litestar(
