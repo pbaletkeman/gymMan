42db35f81b8f94474f5abcdae4350e14e1b5ab77
Pete Letkeman <pete@letkeman.ca>
2024-02-26 19:36:47 -0500

    code clean up

--- controllers/exercise_controller.py
+++ controllers/exercise_controller.py
@@ -6,11 +6,11 @@ from litestar.exceptions import HTTPException
 from litestar.pagination import OffsetPagination
 from litestar.repository.filters import OrderBy
 
-from litestar import get, HttpMethod, route, status_codes
+from litestar import get, status_codes
 from litestar.contrib.sqlalchemy.repository import SQLAlchemyAsyncRepository
 from litestar.controller import Controller
 from litestar.di import Provide
-from litestar.handlers.http_handlers.decorators import delete, post
+from litestar.handlers.http_handlers.decorators import delete, post, put, patch
 from litestar.params import Parameter
 from litestar.repository.filters import LimitOffset
 from pydantic import TypeAdapter
@@ -23,7 +23,7 @@ from logger import logger
 
 
 class ExerciseRepository(SQLAlchemyAsyncRepository[Exercise]):
-    """Exercise repository."""
+    """Exercise repository"""
 
     model_type = Exercise
 
@@ -32,29 +32,6 @@ async def provide_exercise_repo(db_session: AsyncSession) -> ExerciseRepository:
     """This provides a simple example demonstrating how to override the join options
     for the repository."""
     return ExerciseRepository(session=db_session)
-
-
-def provide_limit_offset_pagination(
-    current_page: int = Parameter(ge=1, query="currentPage", default=1, required=False),
-    page_size: int = Parameter(
-        query="pageSize",
-        ge=1,
-        default=10,
-        required=False,
-    ),
-) -> LimitOffset:
-    """Add offset/limit pagination.
-
-    Return type consumed by `Repository.apply_limit_offset_pagination()`.
-
-    Parameters
-    ----------
-    current_page : int
-        LIMIT to apply to select.
-    page_size : int
-        OFFSET to apply to select.
-    """
-    return LimitOffset(page_size, page_size * (current_page - 1))
 
 
 class ExerciseController(Controller):
@@ -70,7 +47,7 @@ class ExerciseController(Controller):
             exercise_repo: ExerciseRepository,
             limit_offset: LimitOffset,
     ) -> OffsetPagination[ExerciseDTO]:
-        """## List exercise items."""
+        """## List Exercise Items"""
         try:
             order_by1 = OrderBy(field_name=Exercise.name)
             results, total = await exercise_repo.list_and_count(limit_offset, order_by1)
@@ -88,9 +65,9 @@ class ExerciseController(Controller):
     async def get_exercise_details(self,
                                    exercise_repo: ExerciseRepository,
                                    exercise_id: int = Parameter(title='Exercise ID',
-                                                                description='The exercise to update.', ),
+                                                                description='Primary Key Of The Exercise To Update.', ),
                                    ) -> ExerciseDTO:
-        """## Get details of an Exercise record."""
+        """## Get Details Of An Exercise Record"""
         try:
             obj = await exercise_repo.get_one(id=exercise_id)
             return ExerciseDTO.model_validate(obj)
@@ -100,7 +77,7 @@ class ExerciseController(Controller):
     @post(tags=exercise_controller_tag)
     async def create_exercise(self, exercise_repo: ExerciseRepository,
                               data: ExerciseCreate, ) -> ExerciseDTO:
-        """## Create a new exercise."""
+        """## Create A New Exercise"""
         try:
             _data = data.model_dump(exclude_unset=True, by_alias=False, exclude_none=True)
             obj = await exercise_repo.add(Exercise(**_data))
@@ -109,16 +86,31 @@ class ExerciseController(Controller):
         except Exception as ex:
             raise HTTPException(detail=str(ex), status_code=status_codes.HTTP_404_NOT_FOUND)
 
-    @route('/{exercise_id:int}',
-           http_method=[HttpMethod.PUT, HttpMethod.PATCH],
-           tags=exercise_controller_tag)
-    async def update_exercise(
+    @put('/{exercise_id:int}', tags=exercise_controller_tag)
+    async def update_exercise_put(
             self,
             exercise_repo: ExerciseRepository,
             data: ExerciseCreate,
-            exercise_id: int = Parameter(title='Exercise ID', description='The exercise to update.', ),
+            exercise_id: int = Parameter(title='Exercise ID', description='Primary Key Of The Exercise To Update.', ),
     ) -> ExerciseCreate:
-        """## Update an exercise."""
+        """## Update An Exercise"""
+        try:
+            _data = data.model_dump(exclude_unset=False, exclude_none=False)
+            _data.update({'id': exercise_id})
+            obj = await exercise_repo.update(Exercise(**_data))
+            await exercise_repo.session.commit()
+            return ExerciseCreate.model_validate(obj)
+        except Exception as ex:
+            raise HTTPException(detail=str(ex), status_code=status_codes.HTTP_404_NOT_FOUND)
+
+    @patch('/{exercise_id:int}', tags=exercise_controller_tag)
+    async def update_exercise_patch(
+            self,
+            exercise_repo: ExerciseRepository,
+            data: ExerciseCreate,
+            exercise_id: int = Parameter(title='Exercise ID', description='Primary Key Of The Exercise To Update.', ),
+    ) -> ExerciseCreate:
+        """## Update An Exercise"""
         try:
             _data = data.model_dump(exclude_unset=True, exclude_none=True)
             _data.update({'id': exercise_id})
@@ -128,16 +120,17 @@ class ExerciseController(Controller):
         except Exception as ex:
             raise HTTPException(detail=str(ex), status_code=status_codes.HTTP_404_NOT_FOUND)
 
-    @delete('/{exercise_id:int}', tags=exercise_controller_tag)
-    async def delete_exercise_tag(
+    @delete('/delete/{exercise_id:str}', tags=exercise_controller_tag)
+    async def delete_exercise(
             self,
             exercise_repo: ExerciseRepository,
-            exercise_id: int = Parameter(title='Exercise ID',
-                                         description='The exercise to delete.', ),
+            exercise_id: str = Parameter(title='Exercise IDs',
+                                         description='Comma Separated List Primary Key Of The Exercises to Delete.', ),
     ) -> None:
-        """## Delete an exercise from the system."""
+        """## Delete Exercise From The System"""
         try:
-            _ = await exercise_repo.delete(exercise_id)
+            _ = await exercise_repo.delete_many(exercise_id.split(','))
+            # _ = await exercise_repo.delete(exercise_id)
             await exercise_repo.session.commit()
         except Exception as ex:
             raise HTTPException(detail=str(ex), status_code=status_codes.HTTP_404_NOT_FOUND)
--- main.py
+++ main.py
@@ -8,14 +8,14 @@ from litestar.contrib.mako import MakoTemplateEngine
 from litestar.di import Provide
 from litestar.openapi import OpenAPIConfig, OpenAPIController
 from litestar.params import Parameter
-from litestar.repository._filters import LimitOffset
+from litestar.repository.filters import LimitOffset
 from litestar.static_files import StaticFilesConfig
 from litestar.template import TemplateConfig
 
 from litestar.contrib.sqlalchemy.plugins import AsyncSessionConfig, SQLAlchemyAsyncConfig, SQLAlchemyInitPlugin
 import logging
 
-from controllers.exercise_controller import ExerciseController, provide_limit_offset_pagination
+from controllers.exercise_controller import ExerciseController
 from controllers.my_controller import MyAPIController
 from models.base_model import Base
 from models.exercise import Exercise
@@ -48,6 +48,29 @@ async def on_startup() -> None:
     """Initializes the database."""
     async with sqlalchemy_config.get_engine().begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
+
+
+def provide_limit_offset_pagination(
+        current_page: int = Parameter(ge=1, query="currentPage", default=1, required=False),
+        page_size: int = Parameter(
+            query="pageSize",
+            ge=1,
+            default=10,
+            required=False,
+        ),
+) -> LimitOffset:
+    """Add offset/limit pagination.
+
+    Return type consumed by `Repository.apply_limit_offset_pagination()`.
+
+    Parameters
+    ----------
+    current_page : int
+        LIMIT to apply to select.
+    page_size : int
+        OFFSET to apply to select.
+    """
+    return LimitOffset(page_size, page_size * (current_page - 1))
 
 
 app = Litestar(
